a76de38737297204f45dd5667243860c82d57230
apiepho <alpiepho@gmail.com>
2019-08-29 15:10:29 -0600

    cleaned up sections; partway thru podcasts

--- tools/convert_from_portfolio2.js
+++ tools/convert_from_portfolio2.js
@@ -2,8 +2,6 @@
 var site = require('./siteConfig')
 const fs = require('fs')
 
-console.log("# Converting data from project-portfolio2...")
-
 // console.log("")
 // console.log("site.jobs: ")
 // console.log("<ul class=\"timeline\">")
@@ -38,8 +36,85 @@ console.log("# Converting data from project-portfolio2...")
 // console.log("")
 
 
-let fake_day = 1
 
+
+
+// let fake_day = 1
+// function getMonthNumber(s) {
+//     if (s === 'jan') return '01'
+//     if (s === 'feb') return '02'
+//     if (s === 'mar') return '03'
+//     if (s === 'apr') return '04'
+//     if (s === 'may') return '05'
+//     if (s === 'jun') return '06'
+//     if (s === 'jul') return '07'
+//     if (s === 'aug') return '08'
+//     if (s === 'sep') return '09'
+//     if (s === 'oct') return '10'
+//     if (s === 'nov') return '11'
+//     if (s === 'dec') return '12'
+//     return '01'
+// }
+
+// for (i=0; i<site.projects.length; i++) {
+//     let project = site.projects[i]
+
+//     let filename = ''
+//     let datestring = ''
+//     datestring += project.begin.year
+//     datestring += '-'
+//     datestring += getMonthNumber(project.begin.month)
+//     datestring += '-'
+//     datestring += fake_day
+//     filename += datestring
+//     fake_day += 1
+//     filename += '-project-'
+//     filename += i+1
+//     filename += '.markdown'
+
+//     let content = ''
+//     content += '---\n'
+//     content += 'type: project\n'
+//     content += 'title: ' + project.title + '\n'
+//     content += 'subtitle: ' + project.type + '\n'
+//     content += 'layout: default\n'
+//     content += 'modal-id: ' + (i+100) + '\n'
+//     content += 'date: ' + datestring + '\n'
+//     let image = project.image.replace('./images/', '')
+//     console.log('cp ~/Projects/gatsby/project-portfolio2/static/images/' + image + ' ../img/portfolio')
+//     let thumb = image
+//     thumb = thumb.replace('.jpeg', '--thumbnail.jpeg')
+//     thumb = thumb.replace('.jpg', '--thumbnail.jpg')
+//     thumb = thumb.replace('.png', '--thumbnail.png')
+
+//     console.log('cp ~/Projects/gatsby/project-portfolio2/static/images/' + image + ' ../img/portfolio/' + thumb)
+
+//     content += 'img: ' + image + '\n'
+//     content += 'thumbnail: ' + thumb + '\n'
+//     content += 'alt: image-alt\n'
+
+//     content += 'project-date: ' + datestring + '\n'
+
+//     content += 'client: '
+//     content += 'Start Bootstrap'
+//     content += '\n'
+
+//     content += 'category: Web Development\n'
+
+
+//     content += 'description: ' + project.description + '\n'
+
+//     content += 'link: ' + project.link + '\n'
+
+//     content += '---\n'
+ 
+
+//     //console.log('# ' + filename + '...')
+//     fs.writeFileSync(filename, content)
+// }
+
+
+let fake_day = 1
 function getMonthNumber(s) {
     if (s === 'jan') return '01'
     if (s === 'feb') return '02'
@@ -56,34 +131,33 @@ function getMonthNumber(s) {
     return '01'
 }
 
-for (i=0; i<site.projects.length; i++) {
-    let project = site.projects[i]
+for (i=0; i<site.podcasts.length; i++) {
+    let podcast = site.podcasts[i]
 
     let filename = ''
     let datestring = ''
-    datestring += project.begin.year
+    datestring += '2019' //podcast.begin.year
     datestring += '-'
-    datestring += getMonthNumber(project.begin.month)
+    datestring += '01' //getMonthNumber(podcast.begin.month)
     datestring += '-'
     datestring += fake_day
     filename += datestring
     fake_day += 1
-    filename += '-project-'
+    filename += '-podcast-'
     filename += i+1
     filename += '.markdown'
 
     let content = ''
     content += '---\n'
-    content += 'title: ' + project.title + '\n'
-    content += 'subtitle: ' + project.type + '\n'
+    content += 'type: podcast\n'
+    content += 'title: ' + podcast.title + '\n'
+    content += 'subtitle: ' + podcast.type + '\n'
     content += 'layout: default\n'
-    content += 'modal-id: ' + (i+100) + '\n'
+    content += 'modal-id: ' + (i+200) + '\n'
     content += 'date: ' + datestring + '\n'
-    let image = 'dreams.png'
-    let thumb = 'dreams-thumbnail.png'
-    image = project.image.replace('./images/', '')
+    let image = podcast.image.replace('./images/', '')
     console.log('cp ~/Projects/gatsby/project-portfolio2/static/images/' + image + ' ../img/portfolio')
-    thumb = image
+    let thumb = image
     thumb = thumb.replace('.jpeg', '--thumbnail.jpeg')
     thumb = thumb.replace('.jpg', '--thumbnail.jpg')
     thumb = thumb.replace('.png', '--thumbnail.png')
@@ -103,9 +177,9 @@ for (i=0; i<site.projects.length; i++) {
     content += 'category: Web Development\n'
 
 
-    content += 'description: ' + project.description + '\n'
+    content += 'description: ' + podcast.description + '\n'
 
-    content += 'link: ' + project.link + '\n'
+    content += 'link: ' + podcast.link + '\n'
 
     content += '---\n'
  
@@ -114,36 +188,10 @@ for (i=0; i<site.projects.length; i++) {
     fs.writeFileSync(filename, content)
 
 }
-console.log('# done.')
 
 
-// title: "Simple Score VB (PWA)",
-// begin: {
-//   month: 'aug',
-//   year: '2019'
-// },
-// type: "personal",
-// image: "./images/screenshot-vb-pwa.png",
-// link: "https://github.com/alpiepho/simple-score-volleyball-pwa/",
-// description: "A Gatsby-based PWA to score Volleyball with and option to send text message, using Netlify Lambda and Twilio on the backend."
-
-
-// Want project to be like:
-
-
-// and contents of 2014-07-18-project-1.markdown:
-// ---
-// title: Dreams
-// subtitle: Website Design
-// layout: default
-// modal-id: 1
-// date: 2014-07-18
-// img: dreams.png
-// thumbnail: dreams-thumbnail.png
-// alt: image-alt
-// project-date: April 2014
-// client: Start Bootstrap
-// category: Web Development
-// description: Lorem ipsum dolor sit amet, usu cu alterum nominavi lobortis. At duo novum diceret. Tantas apeirian vix et, usu sanctus postulant inciderint ut, populo diceret necessitatibus in vim. Cu eum dicam feugiat noluisse.
-
-// ---
+// type: 'history',
+// title: 'Commandline Heros',
+// description: 'An original podcast about the people who transform technology from the command line up.',
+// image: 'https://alpiepho.github.io/project-portfolio2/images/screenshot-commandline-heros.png',
+// link: 'https://www.redhat.com/en/command-line-heroes'
